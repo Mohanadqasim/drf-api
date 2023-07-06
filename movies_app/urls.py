9b9ea530bcf8a0a2de6af9b20b5f9b291cf34697
mohanadqasim <mohanad.qasim@hotmail.com>
2023-07-06 17:36:28 +0300

    done

--- movies_app/urls.py
+++ movies_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import MoviesList,MoviesDetail
 # from .views import GameList,GameDetail
 
 urlpatterns=[
-    path('',),
-    path('/<int:pk>/')
+    path('',MoviesList.as_view(),name='movies_list'),
+    path('/<int:pk>/',MoviesDetail.as_view(),name='movie_detail')
 
 ]
--- movies_app/views.py
+++ movies_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Movie
+from .serializers import MovieSerializer
 # Create your views here.
+class MoviesList(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MoviesDetail(generics.RetrieveUpdateDestroyAPIView): #RetrieveAPIView, RetrieveUpdateAPIView
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
